57e25894275cd62b7317f22a435945e5203f407e
Araxielfenix <araxielfenix@outlook.com>
2024-11-19 00:40:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,95 @@
-async function getStreamInfo(channel) {
-    const urls = [
-        `https://decapi.me/twitch/title/${channel}`,
-        `https://decapi.me/twitch/game/${channel}`,
-        `https://decapi.me/twitch/viewercount/${channel}`,
-    ];
+import express from 'express';
+import fs from 'fs';
+import ws from 'ws';
+import expressWs from 'express-ws';
+import {job} from './keep_alive.js';
+import {OpenAIOperations} from './openai_operations.js';
+import {TwitchBot} from './twitch_bot.js';
 
-    try {
-        const [titleResponse, gameResponse, viewerResponse] = await Promise.all(urls.map(url => fetch(url)));
+// Start keep alive cron job
+job.start();
+console.log(process.env);
 
-        if (!titleResponse.ok || !gameResponse.ok || !viewerResponse.ok) {
-            throw new Error('Network response was not ok');
-        }
+// Setup express app
+const app = express();
+const expressWsInstance = expressWs(app);
 
-        const titulo = await titleResponse.text();
-        const categoria = await gameResponse.text();
-        const espectadores = await viewerResponse.text();
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
 
-        return { titulo, categoria, espectadores };
-    } catch (error) {
-        console.error('Error al obtener la información del stream:', error);
-        return null; // Maneja el error devolviendo null
-    }
+// Load environment variables
+const GPT_MODE = process.env.GPT_MODE || 'CHAT';
+const HISTORY_LENGTH = process.env.HISTORY_LENGTH || 20;
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY || '';
+const MODEL_NAME = process.env.MODEL_NAME || 'google/gemma-2-9b-it:free';
+const TWITCH_USER = process.env.TWITCH_USER || 'RodentPlay';
+const TWITCH_AUTH = process.env.TWITCH_AUTH || 'oauth:a34lxh7cbszmea7icbyxhtyeinvyoo';
+const COMMAND_NAME = process.env.COMMAND_NAME || '@RodentPlay';
+const CHANNELS = process.env.CHANNELS || 'AraxielFenix, Maritha_F, FooNess13, nunchuckya , soyyonotuxdjsjs';
+const SEND_USERNAME = process.env.SEND_USERNAME || 'true';
+const ENABLE_TTS = process.env.ENABLE_TTS || 'false';
+const ENABLE_CHANNEL_POINTS = process.env.ENABLE_CHANNEL_POINTS || 'false';
+const COOLDOWN_DURATION = parseInt(process.env.COOLDOWN_DURATION, 10) || 10; // Cooldown duration in seconds
+
+if (!OPENAI_API_KEY) {
+    console.error('No OPENAI_API_KEY found. Please set it as an environment variable.');
 }
 
-// Modifica el evento onMessage para usar getStreamInfo()
+const commandNames = COMMAND_NAME.split(',').map(cmd => cmd.trim().toLowerCase());
+const channels = CHANNELS.split(',').map(channel => channel.trim());
+const maxLength = 399;
+let fileContext = 'You are a helpful Twitch Chatbot.';
+let lastUserMessage = '';
+let lastResponseTime = 0; // Track the last response time
+let canal = 'AraxielFenix';
+
+// Setup Twitch bot
+console.log('Channels: ', channels);
+const bot = new TwitchBot(TWITCH_USER, TWITCH_AUTH, channels, OPENAI_API_KEY, ENABLE_TTS);
+
+// Setup OpenAI operations
+fileContext = fs.readFileSync('./file_context.txt', 'utf8');
+const toDay = new Date();
+const horaCdmx = toDay.toLocaleString("es-MX", {timeZone: "America/Mexico_City"});
+
+console.log(`La fecha y hora en la Ciudad de México es: ${horaCdmx}`);
+
+fileContext += '\n La fecha y hora actual en la ciudad de México es: ' + horaCdmx;
+fileContext += streamInfo(canal);
+fileContext += '\nPor favor, responde el mensaje del espectador:';
+
+const openaiOps = new OpenAIOperations(fileContext, OPENAI_API_KEY, MODEL_NAME, HISTORY_LENGTH);
+
+// Setup Twitch bot callbacks
+bot.onConnected((addr, port) => {
+    console.log(`* Connected to ${addr}:${port}`);
+    channels.forEach(channel => {
+        console.log(`* Joining ${channel}`);
+        console.log(`* Saying hello in ${channel}`);
+    });
+});
+
+bot.onDisconnected(reason => {
+    console.log(`Disconnected: ${reason}`);
+});
+
+// Connect bot
+bot.connect(
+    () => {
+        console.log('Bot connected!');
+    },
+    error => {
+        console.error('Bot couldn\'t connect!', error);
+    }
+);
+
 bot.onMessage(async (channel, user, message, self) => {
     if (self) return;
 
     const currentTime = Date.now();
-    const elapsedTime = (currentTime - lastResponseTime) / 1000; // Tiempo en segundos
+    const elapsedTime = (currentTime - lastResponseTime) / 1000; // Time in seconds
+    
+    canal = channel;
 
     if (ENABLE_CHANNEL_POINTS === 'true' && user['msg-id'] === 'highlighted-message') {
         console.log(`Highlighted message: ${message}`);
@@ -36,12 +97,7 @@ bot.onMessage(async (channel, user, message, self) => {
             bot.say(channel, `PoroSad Por favor, espera ${COOLDOWN_DURATION - elapsedTime.toFixed(1)} segundos antes de enviar otro mensaje. NotLikeThis`);
             return;
         }
-        lastResponseTime = currentTime;
-
-        const streamInfo = await getStreamInfo(channel.replace('#', '')); // Obtener información del stream
-        if (streamInfo) {
-            fileContext += `\nMensaje recibido en el canal: ${channel}, titulo del stream: ${streamInfo.titulo}, categoria del stream: ${streamInfo.categoria}, cantidad de espectadores: ${streamInfo.espectadores}.\n`;
-        }
+        lastResponseTime = currentTime; // Update the last response time
 
         const response = await openaiOps.make_openrouter_call(message);
         bot.say(channel, response);
@@ -53,16 +109,11 @@ bot.onMessage(async (channel, user, message, self) => {
             bot.say(channel, `PoroSad Por favor, espera ${COOLDOWN_DURATION - elapsedTime.toFixed(1)} segundos antes de enviar otro mensaje. NotLikeThis`);
             return;
         }
-        lastResponseTime = currentTime;
+        lastResponseTime = currentTime; // Update the last response time
 
         let text = message.slice(command.length).trim();
         if (SEND_USERNAME === 'true') {
             text = `Message from user ${user.username}: ${text}`;
-        }
-
-        const streamInfo = await getStreamInfo(channel.replace('#', '')); // Obtener información del stream
-        if (streamInfo) {
-            fileContext += `\nMensaje recibido en el canal: ${channel}, titulo del stream: ${streamInfo.titulo}, categoria del stream: ${streamInfo.categoria}, cantidad de espectadores: ${streamInfo.espectadores}.\n`;
         }
 
         const response = await openaiOps.make_openrouter_call(text);
@@ -87,3 +138,102 @@ bot.onMessage(async (channel, user, message, self) => {
         }
     }
 });
+
+app.ws('/check-for-updates', (ws, req) => {
+    ws.on('message', message => {
+        // Handle WebSocket messages (if needed)
+    });
+});
+
+const messages = [{role: 'system', content: 'You are a helpful Twitch Chatbot.'}];
+console.log('GPT_MODE:', GPT_MODE);
+console.log('History length:', HISTORY_LENGTH);
+console.log('OpenAI API Key:', OPENAI_API_KEY);
+console.log('Model Name:', MODEL_NAME);
+
+app.use(express.json({extended: true, limit: '1mb'}));
+app.use('/public', express.static('public'));
+
+app.all('/', (req, res) => {
+    console.log('Received a request!');
+    res.render('pages/index');
+});
+
+if (GPT_MODE === 'CHAT') {
+    fs.readFile('./file_context.txt', 'utf8', (err, data) => {
+        if (err) throw err;
+        console.log('Reading context file and adding it as system-level message for the agent.');
+        messages[0].content = data;
+    });
+} else {
+    fs.readFile('./file_context.txt', 'utf8', (err, data) => {
+        if (err) throw err;
+        console.log('Reading context file and adding it in front of user prompts:');
+        fileContext = data;
+    });
+}
+
+app.get('/gpt/:text', async (req, res) => {
+    const text = req.params.text;
+
+    let answer = '';
+    try {
+        if (GPT_MODE === 'CHAT') {
+            answer = await openaiOps.make_openrouter_call(text);
+        } else if (GPT_MODE === 'PROMPT') {
+            const prompt = `${fileContext}\n\nUser: ${text}\nAgent:`;
+            answer = await openaiOps.make_openrouter_call_completion(prompt);
+        } else {
+            throw new Error('GPT_MODE is not set to CHAT or PROMPT. Please set it as an environment variable.');
+        }
+
+        res.send(answer);
+    } catch (error) {
+        console.error('Error generating response:', error);
+        res.status(500).send('An error occurred while generating the response.');
+    }
+});
+
+const server = app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
+
+const wss = expressWsInstance.getWss();
+wss.on('connection', ws => {
+    ws.on('message', message => {
+        // Handle client messages (if needed)
+    });
+});
+
+function notifyFileChange() {
+    wss.clients.forEach(client => {
+        if (client.readyState === ws.OPEN) {
+            client.send(JSON.stringify({updated: true}));
+        }
+    });
+}
+
+async function getStreamInfo(channel) {
+    const urls = [
+        `https://decapi.me/twitch/title/${channel}`,
+        `https://decapi.me/twitch/game/${channel}`,
+        `https://decapi.me/twitch/viewercount/${channel}`,
+    ];
+
+    try {
+        const [titleResponse, gameResponse, viewerResponse] = await Promise.all(urls.map(url => fetch(url)));
+
+        if (!titleResponse.ok || !gameResponse.ok || !viewerResponse.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const titulo = await titleResponse.text();
+        const categoria = await gameResponse.text();
+        const espectadores = await viewerResponse.text();
+
+        return `\nMensaje recibido en el canal: ${channel}, titulo del stream: ${streamInfo.titulo}, categoria del stream: ${streamInfo.categoria}, cantidad de espectadores: ${streamInfo.espectadores}.\n`;
+    } catch (error) {
+        console.error('Error al obtener la información del stream:', error);
+        return null; // Maneja el error devolviendo null
+    }
+}
